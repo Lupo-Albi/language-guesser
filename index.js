beadc61c8a005d59b15dd803fa71b057e5ea0bb1
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-05-02 15:12:00 -0300

    Installed packages and trying to make it work

---
+++ index.js
@@ -0,0 +1,9 @@
+const franc = require('franc');
+const langs = require('langs');
+
+const sentence = process.argv.slice(2);
+
+const langCode = franc("I'm going to the chuch tomorrow");
+
+const langObj = langs.where("3", codigo);
+console.log(langObj.name);
