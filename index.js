62b8f007e6658d1fc526d3fe7ffcad5d0cb894dd
Lupo-Albi <luandersonalbuquerque@gmail.com>
2021-05-04 20:25:44 -0300

    Completed exercise

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const franc = require('franc');
 const langs = require('langs');
+const colors = require('colors');
 
-const sentence = process.argv.slice(2);
-
-const langCode = franc("I'm going to the chuch tomorrow");
-
-const langObj = langs.where("3", codigo);
-console.log(langObj.name);
+const sentence = process.argv.slice(2).join(' ');
+const langCode = franc(sentence);
+const langObj = langs.where("3", langCode);
+// There are cases where the language code exists but it does not match any code in the langs package
+if(langCode === 'und' || !langObj) {
+    console.log("Could not match a language. Please try again with a larger sample.".red);
+} else {
+    console.log(`Our best guess is: ${langObj.name}`.green);
+}
